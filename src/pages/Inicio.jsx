dcd071583eae2f1c661efb7fc463efbdeb791555
peidofofo123 <mikaellysilvalima16@gmail.com>
2024-06-04 08:20:41 -0300

    alteração arq inicio

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -6,6 +6,7 @@ const Home = () => {
 
     return (
         <div>
+            <h2>Titulo da página</h2>
            <p>Outra tela Inicial</p>
            <img className='img-fluid' src={logo}
            alt='logo'></img>
